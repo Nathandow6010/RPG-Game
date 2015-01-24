da37617af950dd563e3d20c4802704dd4572fbf0
Nathandow6010 <nathandow1@hotmail.co.uk>
2015-01-24 12:35:11 +0000

    Added Movement
    need to fix Level 1 code for script

---
+++ RPG Game/main.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+
+
+char Map[10][10] = {"#########",
+                    "#(   #  #",
+                    "# #  #  #",
+                    "# ####  #",
+                    "# #     #",
+                    "# # #####",
+                    "#   #!  #",
+                    "# ##### #",
+                    "#       #",
+                    "#########" };
+
+int Gamespeed = 100;
+int Level = 0;
+bool stopgame = false;
+
+int main()
+{
+
+    while(stopgame == false && Level == 0)
+    {
+      system("cls");
+      for(int y = 0; y < 10; y++)
+      {
+          cout << Map[y] << endl;
+      }
+        for (int y = 0; y<10; y++)
+        {
+          for(int x = 0; x<10; x++)
+          {
+
+             switch(Map[y][x])
+              {
+              case '(':
+                {
+                    if(GetAsyncKeyState(VK_UP) != 0)
+                    {
+                        int y2 = (y-1);
+
+                        switch(Map[y2][x])
+                        {
+                        case ' ':
+                            {
+                                Map[y][x] = ' ';
+                                y -= 1;
+                                Map[y2][x] = '(';
+                            }break;
+                        case '!':
+                            {
+                                Level = 1;
+                            }break;
+                        }
+                    }
+
+                    if(GetAsyncKeyState(VK_DOWN) != 0)
+                    {
+                        int y2 = (y + 1);
+
+                        switch(Map[y2][x])
+                        {
+                        case ' ':
+                            {
+                                Map[y][x] = ' ';
+                                y += 1;
+                                Map[y2][x] = '(';
+                            }break;
+                        case '!':
+                            {
+                                Level = 1;
+                            }
+                        }
+                    }
+
+                    if (GetAsyncKeyState(VK_RIGHT) != 0)
+                    {
+                        int x2 = (x + 1);
+
+                        switch(Map[y][x2])
+                        {
+                        case ' ':
+                            {
+                                Map[y][x] = ' ';
+                                x += 1;
+                                Map[y][x2] = '(';
+                            }break;
+                        case '!':
+                            {
+                                Level = 1;
+                            }
+                        }
+                    }
+
+                    if (GetAsyncKeyState(VK_LEFT) != 0)
+                    {
+                        int x2 = (x - 1);
+
+                        switch(Map[y][x2])
+                        {
+                        case ' ':
+                            {
+                                Map[y][x] = ' ';
+                                x -= 1;
+                                Map[y][x2] = '(';
+                            }break;
+                        case '!':
+                            {
+                                Level = 1;
+                            }
+                        }
+                    }
+                }
+              }
+          }
+      }
+      Sleep(Gamespeed);
+    }
+    while (stopgame == false && Level = 1)
+    {
+        cout << "LEVEL 1/n/n";
+        system("pause");
+    }
+}
